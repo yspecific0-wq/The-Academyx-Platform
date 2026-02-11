c7ece50c67ccee2f6fef34490e2669d1c2822128
yspecific0-wq <yspecific0@gmail.com>
2026-02-11 22:04:09 +0530

    change

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -130,7 +130,10 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://the-academyx-platform.vercel.app",
+    "http://localhost:5173",
+]
 
 
 # Static files (CSS, JavaScript, Images)
